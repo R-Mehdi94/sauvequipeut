14c3b89e36938a9566f60b1b7e9f3ec33d10b7e9
R-Mehdi94 <mehdi.bellilpro@gmail.com>
2024-12-24 20:08:16 +0100

    test communication avec le server prof

---
+++ target/debug/build/serde-3cccdaca8051661b/build_script_build-3cccdaca8051661b.d
@@ -0,0 +1,5 @@
+/home/mehdi/ESGI/4AL/rust/sauvequipeut/target/debug/build/serde-3cccdaca8051661b/build_script_build-3cccdaca8051661b: /home/mehdi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.216/build.rs
+
+/home/mehdi/ESGI/4AL/rust/sauvequipeut/target/debug/build/serde-3cccdaca8051661b/build_script_build-3cccdaca8051661b.d: /home/mehdi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.216/build.rs
+
+/home/mehdi/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde-1.0.216/build.rs:
---
+++ target/debug/deps/server-13339bd9421ee937.d
@@ -0,0 +1,5 @@
+/home/mehdi/ESGI/4AL/rust/sauvequipeut/target/debug/deps/server-13339bd9421ee937: server/src/main.rs
+
+/home/mehdi/ESGI/4AL/rust/sauvequipeut/target/debug/deps/server-13339bd9421ee937.d: server/src/main.rs
+
+server/src/main.rs:
